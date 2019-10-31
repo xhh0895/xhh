e6116887373f392ed8f8811fd1bf4f95d1dfbed1
xhh <xhh0895@163.com>
2019-10-31 16:35:57 +0800

    qw Please enter the commit message for your changes. Lines starting

---
+++ python/listpy.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+from datetime import datetime
+import base64
+'''
+中文
+'''
+# print(datetime.now())
+
+# cday = datetime.strptime('2018-02-13 11:11:11','%Y-%m-%d %H:%M:%S')
+# print(cday)
+
+s = base64.b64encode('在Python中使用BASE 64编码'.encode('utf-8'))
+print(s)
+d = base64.b64decode(s).decode('utf-8')
+print(d)
+
+s = base64.urlsafe_b64encode('在Python中使用BASE 64编码'.encode('utf-8'))
+print(s)
+d = base64.urlsafe_b64decode(s).decode('utf-8')
+print(d)
